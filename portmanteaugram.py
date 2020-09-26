c6f91aebf63ed99d575786b7db84e66af8067085
Garrett DiCenzo <59700373+gdicenzo@users.noreply.github.com>
2020-09-26 18:32:29 -0400

    Added second half of program code

--- portmanteaugram.py
+++ portmanteaugram.py
@@ -7,3 +7,10 @@ for i in range(len(name) - 2, -1, -1):
     if name[i] in vowels:
         port = name[:i + 1]
         break
+
+for i in range(1, len(other) - 1):
+    if other[i] in vowels:
+        manteau = other[i + 1:]
+        break
+
+print(port + manteau)
