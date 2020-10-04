c1d1ddea26989689afe1efa402f9bf7659944af7
Tianyi Zheng <tianyizheng02@gmail.com>
2020-10-04 23:56:32 +0000

    Fix edge case where portmanteau element would be undefined if input started with a vowel

--- portmanteaugram.py
+++ portmanteaugram.py
@@ -2,15 +2,21 @@ name = input("Please enter your name: ")
 other = input("Please enter another word: ")
 
 vowels = ['a', 'e', 'i', 'o', 'u']
+port = ""
+manteau = ""
 
 for i in range(len(name) - 2, -1, -1):
     if name[i] in vowels:
         port = name[:i + 1]
         break
-
+if port == "":
+    port = name
+        
 for i in range(1, len(other) - 1):
     if other[i] in vowels:
         manteau = other[i + 1:]
         break
+if manteau == "":
+    manteau = other
 
 print(port + manteau)
